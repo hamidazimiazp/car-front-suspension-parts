81ad68c1453ce57fbd52754efbc43307078af407
Hamid Azimi <hamidazimi.azp@gmail.com>
2024-09-24 14:28:39 +0330

    improve usetitle hook

--- frontend/shopping/src/router/router.jsx
+++ frontend/shopping/src/router/router.jsx
@@ -4,7 +4,7 @@ import HomePage from "pages/HomePage/HomePage";
 const Router = () => {
   return (
     <Routes>
-      <Route index element={<HomePage />} />
+      <Route index element={<HomePage PageTitle="صفحه اصلی" />} />
     </Routes>
   );
 };
