b08a40681a52a64c9695918f9afd408300405264
Hamid Azimi <hamidazimi.azp@gmail.com>
2024-09-24 14:31:26 +0330

    use propTypes

--- frontend/shopping/src/pages/HomePage/HomePage.jsx
+++ frontend/shopping/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,15 @@
 import HomePageTemp from "components/templates/HomePage/HomePageTemp";
 import useTitle from "utils/hooks/useTitle";
+import propTypes from "prop-types";
 
-const HomePage = () => {
-  useTitle("صفحه اصلی");
+const HomePage = ({ PageTitle }) => {
+  useTitle(PageTitle);
 
   return <HomePageTemp />;
 };
 
 export default HomePage;
+
+HomePage.propTypes = {
+  PageTitle: propTypes.string,
+};
