0afa98261bdb8d5b60489ffea8828cafee5d7f51
Hamid Azimi <hamidazimi.azp@gmail.com>
2024-09-25 11:54:41 +0330

    mange dark and light theme in root

--- frontend/shopping/src/App.jsx
+++ frontend/shopping/src/App.jsx
@@ -1,10 +1,23 @@
+import { ThemeProvider } from "@mui/material";
 import Router from "router/Router";
-
+import { darkTheme, lightTheme } from "constants/theme";
+import { useCallback } from "react";
+import { useLocalStorage } from "utils/localStorage/localStorage";
 const App = () => {
+  const [themeValue, setThemeValue] = useLocalStorage("theme", "light");
+
+  const ThemeHandler = useCallback(() => {
+    if (themeValue == "light") {
+      setThemeValue("dark");
+    } else {
+      setThemeValue("light");
+    }
+  }, [setThemeValue, themeValue]);
+
   return (
-    <>
-      <Router />
-    </>
+    <ThemeProvider theme={themeValue === "light" ? lightTheme : darkTheme}>
+      <Router themeValue={themeValue} ThemeHandler={ThemeHandler} />
+    </ThemeProvider>
   );
 };
 
