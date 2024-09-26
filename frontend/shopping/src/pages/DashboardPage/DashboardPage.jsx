b0f002127f9d9339344b70cc879af778c2939203
Hamid Azimi <hamidazimi.azp@gmail.com>
2024-09-26 12:21:13 +0330

    get user details

--- frontend/shopping/src/pages/DashboardPage/DashboardPage.jsx
+++ frontend/shopping/src/pages/DashboardPage/DashboardPage.jsx
@@ -1,9 +1,18 @@
 import useTitle from "utils/hooks/useTitle";
 import propTypes from "prop-types";
 import DashboardPageTemp from "components/templates/DashboardPage/DashboardPageTemp";
+import { useQuery } from "@tanstack/react-query";
+import { whoAmI } from "services/user";
 
 const DashboardPage = ({ PageTitle }) => {
   useTitle(PageTitle);
+
+  const { data, isPending } = useQuery({
+    queryKey: ["whoami"],
+    queryFn: async () => await whoAmI(),
+  });
+
+  console.log(data?.response?.data, isPending);
 
   return <DashboardPageTemp />;
 };
